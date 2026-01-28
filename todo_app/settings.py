74f191d81f2d004eb39c3574d341de651ac2fb26
inv-ashwin <ashwin.sanalkumar@innovature.com>
2026-01-28 10:07:56 +0530

    dotenv update

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9*ue+3kv+nhh99d@b=wcg!*%5!39gghqarp82@&#$fyj8*h!5v'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("DJANGO_SECRET_KEY is not set in .env")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
